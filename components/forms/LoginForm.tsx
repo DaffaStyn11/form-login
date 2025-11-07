fecde4c9f1683dfb990c9fa51de1ae146bd75102
DaffaStyn11 <daffasetyan@gmail.com>
2025-11-07 19:05:40 +0700

    update form

--- components/forms/LoginForm.tsx
+++ components/forms/LoginForm.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -12,9 +13,8 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Eye, EyeOff, Lock, Mail } from "lucide-react";
-import { useState } from "react";
 
-export default function LoginForm() {
+export default function FormLogin() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
